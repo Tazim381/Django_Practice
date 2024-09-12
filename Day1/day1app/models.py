b59cad4b70f4a0e7e7cb851a941d43f0ee99b3f3
seaum ahmed tazim <jucse28.381@gmail.com>
2024-09-12 14:58:03 +0600

    show data using class and object

--- Day1/day1app/models.py
+++ Day1/day1app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+
+class Customer:
+    def __init__(self, id, name, phone_number):
+        self.id = id
+        self.name = name
+        self.phone_number = phone_number
--- Day1/day1app/views.py
+++ Day1/day1app/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Customer
 
 def home(request):
     return render(request,'home.html')
 
 def newpage(request):
-    return HttpResponse("This is new page")
+    customer1 =Customer(381,"Tazim","0176969690")
+    customer2 = Customer(382,"Jakir", "0187823288")
+    customer3 = Customer(320, "Rasel", "0197972397987")
+    customer4 =Customer(381,"Tazim","0176969690")
+    customer5 = Customer(382,"Jakir", "0187823288")
+    customer6 = Customer(320, "Rasel", "0197972397987")
+    customer7 =Customer(381,"Tazim","0176969690")
+    customer8= Customer(382,"Jakir", "0187823288")
+    customer9 = Customer(320, "Rasel", "0197972397987")
+    customer10 =Customer(381,"Tazim","0176969690")
+    customer11= Customer(382,"Jakir", "0187823288")
+    customer12= Customer(320, "Rasel", "0197972397987")
+    customers=[customer1,customer2,customer3,customer4,customer5,customer6,customer7,customer8,customer9,customer10,customer11,customer12]
+    return render(request,"newpage.html",{'customers':customers})
 
 def result(request):
     num1 = int(request.POST['num1'])
